2aae5c41fe9d143593bef065902c610ba4f2f6f6
JaccoJongenotter <22053@ma-web.nl>
2018-03-20 09:14:53 +0100

    Sorted scripts in mapjes

---
+++ Jacco/PERSEUS/Assets/Scripts/AI/PegasusMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PegasusMove : MonoBehaviour
+{
+
+    public float verticalSpeed;
+    public float amplitude;
+    private float turnSpeed = 25f;
+    private float moveSpeed = 5f;
+
+
+
+
+    private Vector3 tempPosition;
+
+    void Update()
+    {
+        Vector3 movement = new Vector3();
+
+        movement -= transform.forward;
+        this.transform.position += (movement * Time.deltaTime * moveSpeed);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+        tempPosition = this.transform.position;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude + 5;
+        transform.position = tempPosition;
+    }
+}
+
--- Jacco/PERSEUS/Assets/Scripts/TestScript.cs
+++ Jacco/PERSEUS/Assets/Scripts/TestScript.cs
@@ -3,55 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestScript : MonoBehaviour {
+    private float turnSpeed = 50f;
+    private float moveSpeed = 10f;
 
 
-    public Transform targetDown;
-    public Transform targetUp;
-    public Transform targetLeft;
-    public Transform targetRight;
-    public Transform targetTopLeft;
-    public float speed;
-    private float axisThreshHold = 0.2f;
     void Update()
     {
-        Vector3 targetDirUp = targetUp.position - transform.position;
-        Vector3 targetDirDown = targetDown.position - transform.position;
-        Vector3 targetDirLeft = targetLeft.position - transform.position;
-        Vector3 targetDirRight = targetRight.position - transform.position;
-        Vector3 targetDirTopLeft = targetTopLeft.position - transform.position;
+        Vector3 movement = new Vector3();
 
-
-        float step = speed * Time.deltaTime;
-        //up
-        if (Input.GetAxis("Vertical") > axisThreshHold)
-        {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirUp, step, 0.0F);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-        //down
-        if (Input.GetAxis("Vertical") < -axisThreshHold)
-        {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirDown, step, 0.0F);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-        //left
-        if (Input.GetAxis("Horizontal") < -axisThreshHold)
-        {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirLeft, step, 0.0F);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-        //right
-        if (Input.GetAxis("Horizontal") > axisThreshHold)
-        {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirRight, step, 0.0F);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-
-        if (Input.GetAxis("Vertical") > axisThreshHold && Input.GetAxis("Horizontal") < -axisThreshHold)
-        {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDirTopLeft, step, 0.0F);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
+        movement -= transform.forward;
+        this.transform.position += (movement * Time.deltaTime * moveSpeed);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
     }
 }
 
